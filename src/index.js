17b241cc58155e7463eae3e678921069b22054ee
MxttVincent <68464368+MxttVincent@users.noreply.github.com>
2021-01-16 16:52:43 +0000

    Introduce roll button and API call to get a result

--- src/index.js
+++ src/index.js
@@ -8,16 +8,36 @@ class DiceArea extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      rollResult: null,
+      rollResult: "Roll the die!",
       diceCount: 1,
       diceSideCount: 6
     };
+  }
+
+  getDiceRoll = () => {
+    fetch("https://rolz.org/api/?1d6.json")
+    .then(res => res.json())
+    .then(
+      (json) => { this.setState({
+        rollResult: json.result
+      });
+      console.log(json.result)
+    },
+      (error) => { this.setState({
+        rollResult: "Error reaching rolling API."
+      })},
+    )
   }
   
   render() {
     return (
       <div className="app">
-        <p>Roll the die!</p>
+        <h1>
+          <p>{this.state.rollResult}</p>
+          <div id="rollButton" onClick={this.getDiceRoll}>
+            <p>Roll</p>
+          </div>
+        </h1>
         <div id="diceArea">
             <Dice />
         </div>
@@ -28,8 +48,6 @@ class DiceArea extends React.Component{
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DiceArea />
-  </React.StrictMode>,
+    <DiceArea />,
   document.getElementById('root')
 );
